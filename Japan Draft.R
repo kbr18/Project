8ed7e8a99062ab12e61105c3945c2a3b3f4a7f68
Jahnvi Jhaveri <60850242+jahnvij@users.noreply.github.com>
2020-02-09 21:18:30 +0000

    Japan 1

--- Japan Draft.R
+++ Japan Draft.R
@@ -82,22 +82,27 @@ JPMKT$Return <- (JPMKT$Close-JPMKT$Open)/JPMKT$Open
 
 #Stock Data
 
+#Amada:
 Amada$Amada.Return <- (Amada$Amada.Close-Amada$Amada.Open)/Amada$Amada.Open
 Amada$RF.Return <- (JPRF$Return)
 Amada$MKT.Return <- (JPMKT$Return)
 
+#Hokuetsu Corporation:
 Hokuetsu$Hokuetsu.Return <- (Hokuetsu$Hokuetsu.Close-Hokuetsu$Hokuetsu.Open)/Hokuetsu$Hokuetsu.Open
 Hokuetsu$RF.Return <- (JPRF$Return)
 Hokuetsu$MKT.Return <- (JPMKT$Return)
 
+#Mitsubishi Motors:
 Mitsubishi$Mitsubishi.Return <- (Mitsubishi$Mitsubishi.Close-Mitsubishi$Mitsubishi.Open)/Mitsubishi$Mitsubishi.Open
 Mitsubishi$RF.Return <- (JPRF$Return)
 Mitsubishi$MKT.Return <- (JPMKT$Return)
 
+#Secom:
 Secom$Secom.Return <- (Secom$Secom.Close-Secom$Secom.Open)/Secom$Secom.Open
 Secom$RF.Return <- (JPRF$Return)
 Secom$MKT.Return <- (JPMKT$Return)
 
+#Tokyu Fudosan Holdings Corporation:
 Tokyu$Tokyu.Return <- (Tokyu$Tokyu.Close-Tokyu$Tokyu.Open)/Tokyu$Tokyu.Open
 Tokyu$RF.Return <- (JPRF$Return)
 Tokyu$MKT.Return <- (JPMKT$Return)
@@ -114,13 +119,15 @@ Amada$Amada.CAR <- cumsum(Amada$Amada.AR)
 
 # Plot CAR
 Amada$Amada.DateNum <- 1:nrow(Amada) 
-plot(Amada.CAR)
+
+plot(Amada$Amada.CAR)
+
 plot(Amada$Amada.Return)
 Amada.EP <- Amada[1:38,] #Creating a subset from the risk-free data, i.e. our Estimation Period, from 9th May to 29th June
 Amada.EW<- Amada[39:88,] #Creating a subset from the risk-free data, i.e. our Event Window, from 2nd July to 7th September
 
-plot(Amada.CAR[1:38])
-plot(Amada.CAR[39:88])
+plot(Amada$Amada.CAR[1:38])
+plot(Amada$Amada.CAR[39:88])
 
 logisticRegression=glm(Amada.CAR~Amada$Amada.Open+Amada$Amada.High+Amada$Amada.Low+Amada$Amada.Volume+Amada$Amada.Adjusted+Amada$Amada.Return, data=Amada.CAR,family=binomial)
 
@@ -128,16 +135,16 @@ Regression <- glm(Amada.CAR ~ MRP, data=Amada.CAR[1:38])
 
 summary(Amada$Amada.CAR)
 
-#Tree time
+#Tree
 
 install.packages("tree")
 install.packages("ISLR")
 library(tree)
 library(ISLR)
 
-High=ifelse(Amada.Return<=0.004384, "No", "Yes")
+High=ifelse(Amada$Amada.Return<=0.004384, "No", "Yes")
 Amada2=data.frame(Amada, High)
-tree.Amada=tree(High~.-Amada.Return, Amada2)
+tree.Amada=tree(High~.-Amada$Amada.Return, Amada2)
 summary(tree.Amada)
 
 Event=ifelse(Amada.CAR<-0.10, "Yes", "No")
@@ -147,4 +154,3 @@ summary(tree.Amada)
 
 plot(Amada.CAR[39:50])
 
-# Trees
